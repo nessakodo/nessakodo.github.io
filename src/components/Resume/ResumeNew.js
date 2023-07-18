ee0155dfaab3c800cbace751b0a680bc95e913d8
nessakodo <nessakodo@gmail.com>
2023-07-18 14:55:24 -0600

    new asset

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/nessakodo/dev/main/src/Assets/NessaKodoResume.pdf";
+  "https://raw.githubusercontent.com/nessakodo/nessakodo.github.io/main/src/Assets/NessaKodoResume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
