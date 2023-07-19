e83fc1a66d332d1858189df84799c6a9531dacb7
nessakodo <nessakodo@gmail.com>
2023-07-19 10:10:21 -0600

    resolving navbar link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -72,7 +72,7 @@ function NavBar() {
             <Nav.Item>
               <Nav.Link
                 as={Link}
-                to="/project"
+                to="/projects"
                 onClick={() => updateExpanded(false)}
               >
                 <AiOutlineFundProjectionScreen
