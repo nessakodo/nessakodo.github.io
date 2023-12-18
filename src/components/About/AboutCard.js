2b209cf8d9b6de6924708aef627e50615a6b98cb
nessakodo <nessakodo@gmail.com>
2023-12-18 16:00:54 -0700

    uhm

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -12,9 +12,23 @@ function AboutCard() {
             and I am based in <span className="purple"> Denver, Colorado.</span>
             <br />
             <br />
-            Apart from coding, here are a few activities I also love!
+            Beyond coding, I enjoy dancing, hiking, and treasured moments with loved ones. I 
+            also dabble in Sonic Pi, a live code music synth based on Ruby, creating techno style beats 
+            as I work to develop one of my current projects, <span className="purple">the Sonic Pibrary.</span>
+            <br></br>
+            <br></br>
+            Leveling up my workflow optimizations and logic design processes is an area where I find
+            limitless opportunities to learn and implement new technologies and tools in my life and 
+            projects. As I continue evolving as a developer, I discover intriguing parallels 
+            across various domains like science, where intersections between science, tech, and humanity fascinate me. 
+             <br></br>
+             <br></br>
+            I graduated from a full-stack software engineering BootCamp in 2022, diving into 
+            computer science shortly afterward, currently pursuing it full-time. As I gaze into 
+            the future, I aspire to achieve my CISSP and delve deeper into a career in cybersecurity, 
+            immersing myself in securing digital landscapes."
           </p>
-          <ul>
+          {/* <ul>
             <li className="about-activity">
               <ImPointRight /> &nbsp; Producing music
             </li>
@@ -24,12 +38,12 @@ function AboutCard() {
             <li className="about-activity"> 
               <ImPointRight /> &nbsp; Adventuring Outside
             </li>
-          </ul>
+          </ul> */}
           <br />
           <p style={{ color: "rgb(155 126 172)" }}>
-          "Light the path to create masterpieces that captivate."{" "}
+          Light the path to create masterpieces that captivate.{" "}
           </p>
-          <footer className="blockquote-footer">Nessa Kodo</footer>
+          {/* <footer className="blockquote-footer">Nessa Kodo</footer> */}
         </blockquote>
       </Card.Body>
     </Card>
