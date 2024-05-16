dd376327f147742e8e8770f86515520ad04af4cf
nessakodo <nessakodo@gmail.com>
2024-05-16 11:41:26 -0600

    fixing errors

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/NessaKodoResume.pdf";
+import pdf from "../../Assets/NessaKodoResume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import { Worker, Viewer } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import { zoomPlugin } from "@react-pdf-viewer/zoom";
+import "@react-pdf-viewer/zoom/lib/styles/index.css";
 
 const resumeLink =
   "https://raw.githubusercontent.com/nessakodo/nessakodo.github.io/main/src/Assets/NessaKodoResume.pdf";
@@ -17,6 +18,8 @@ function ResumeNew() {
   useEffect(() => {
     setWidth(window.innerWidth);
   }, []);
+
+  const zoomPluginInstance = zoomPlugin();
 
   return (
     <div>
@@ -29,27 +32,16 @@ function ResumeNew() {
           To download, click the button below.
         </p>
         <Row style={{ justifyContent: "center", position: "relative" }} className="resume-row">
-          {/* <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button> */}
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
+          <Worker workerUrl={`https://unpkg.com/pdfjs-dist@2.7.570/build/pdf.worker.min.js`}>
+            <Viewer fileUrl={resumeLink} plugins={[zoomPluginInstance]} />
+          </Worker>
         </Row>
 
-
-          <Row style={{ justifyContent: "center", position: "relative" }} className="button-row">
+        <Row style={{ justifyContent: "center", position: "relative" }} className="button-row">
           <Button
-          
             variant="primary"
             href={pdf}
             target="_blank"
